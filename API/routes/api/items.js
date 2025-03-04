4b24eabfc7419521b1a6933a40157cf29edbe203
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2025-03-04 18:06:42 +0000

    fix items/:sku endpoint

--- API/routes/api/items.js
+++ API/routes/api/items.js
@@ -9,9 +9,11 @@ const router = express.Router();
 const PRICELIST_PATH = './files/pricelist.json'
 const ITEM_LIST_PATH = './files/item_list.json';
 
+// Import pg connection instance.
+const { db } = require('./pg-instance.js');
+
 // Get item price by SKU.
 router.get('/:sku', async (req, res) => {
-  let item_found = false;
   let item_object = {};
 
   try {
@@ -25,33 +27,46 @@ router.get('/:sku', async (req, res) => {
     return res.sendStatus(400);
   }
 
-  fs.readFile(PRICELIST_PATH, 'utf8', (err, data) => {
-      if(err) {
-        // req.reject etc.
-        return res.status(400).json({ error: 'Failed to load pricelist.'});
-      }
+  // Get results from the pricelist
+  item_object = await db.oneOrNone('SELECT * FROM pricelist WHERE sku = $1', [req.params.sku]);
 
-    let sku = req.params.sku;
-    data = JSON.parse(data);
+  // Item was not found in the pricelist.
+  if (result === null) {
+    return res.sendStatus(404);
+  }
 
-    // Iterate over each item in the items JSON array.
-    for (const item of data.items) {
-      // Find the requested item.
-      if (item.sku === sku) {
-        item_found = true;
-        item_object = item;
-        break;
-      }
-    }
+  // Item found, send item object as response.
+  return res.status(200).json(item_object);
 
-    // Item found, send item object as response.
-    if(item_found) {
-      return res.status(200).json(item_object);
-    } else {
-      // Item was not found in the pricelist.
-      return res.sendStatus(404);
-    }
-  });
+  // TODO: Convert it to a json
+
+  // fs.readFile(PRICELIST_PATH, 'utf8', (err, data) => {
+  //     if(err) {
+  //       // req.reject etc.
+  //       return res.status(400).json({ error: 'Failed to load pricelist.'});
+  //     }
+
+  //   let sku = req.params.sku;
+  //   data = JSON.parse(data);
+
+  //   // Iterate over each item in the items JSON array.
+  //   for (const item of data.items) {
+  //     // Find the requested item.
+  //     if (item.sku === sku) {
+  //       item_found = true;
+  //       item_object = item;
+  //       break;
+  //     }
+  //   }
+
+  //   // Item found, send item object as response.
+  //   if(item_found) {
+  //     return res.status(200).json(item_object);
+  //   } else {
+  //     // Item was not found in the pricelist.
+  //     return res.sendStatus(404);
+  //   }
+  // });
 });
 
 // Get pricelist.
